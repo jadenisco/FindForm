047564a1fd53ec90fb369137f501e96b9ef3a4c0
John DeNisco <jadenisco17@gmail.com>
2022-09-21 09:13:22 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿//#define MGH
+
+using System;
+
+namespace FindForm {
+    class Program {
+        static void Main() {
+#if MGH
+            String vol_root_dir = "//Cifs2/voldept$";
+            String? pet_volunteer_dir = vol_root_dir + "/.Volunteer Files/Pet Therapy";
+#else
+            String vol_root_dir = "/Users/jdenisco/Developer/Windows/testroot";
+            String? pet_volunteer_root_dir = null;
+#endif
+            String adult_volunteer_root_dir = vol_root_dir + "/.Volunteer Files/ADULT MEDICAL AND NONMEDICAL";
+            String junior_volunteer_root_dir = vol_root_dir + "/.Volunteer Files/JUNIOR MEDICAL AND NONMEDICAL/Active JR Volunteers";
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,14 @@
+# This is a C# Program that will find a covid form
+
+## This project was created with
+
+``` console
+$
+$ dotnet new console --framework net6.0
+$ 
+```
+
+## Some Links
+
+[Tutorial with VSCode](https://docs.microsoft.com/en-us/dotnet/core/tutorials/with-visual-studio-code?pivots=dotnet-6-0#debug)
+[New Console](]https://aka.ms/new-console-template)
