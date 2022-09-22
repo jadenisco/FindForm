7ec650b8c9658c5a157e8644a2dfc19e9370f444
John DeNisco <jadenisco17@gmail.com>
2022-09-22 07:48:05 -0400

    File and Directory Examples

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿//#define MGH
 
 using System;
+using System.IO;
 
 namespace FindForm {
     class Program {
@@ -15,7 +16,32 @@ namespace FindForm {
             String adult_volunteer_root_dir = vol_root_dir + "/.Volunteer Files/ADULT MEDICAL AND NONMEDICAL";
             String junior_volunteer_root_dir = vol_root_dir + "/.Volunteer Files/JUNIOR MEDICAL AND NONMEDICAL/Active JR Volunteers";
 
-            Console.WriteLine("Hello, World!");
+            String path = adult_volunteer_root_dir;
+
+            Console.WriteLine("PATH: '{0}'", path);
+            if(File.Exists(path)) {
+                ProcessFile(path);
+            } else if (Directory.Exists(path)){
+                ProcessDirectory(path);
+
+            }
+        }
+
+        private static void ProcessFile(string path) {
+            Console.WriteLine("    FILE: '{0}'", path);
+        }
+
+        private static void ProcessDirectory(string targetDirectory) {
+            Console.WriteLine("  DIR: '{0}", targetDirectory);
+            string [] fileEntries = Directory.GetFiles(targetDirectory);
+            foreach(string fileName in fileEntries) {
+                ProcessFile(fileName);
+            }
+
+            string [] subdirectoryEntries = Directory.GetDirectories(targetDirectory);
+            foreach(string subdirectory in subdirectoryEntries) {
+                ProcessDirectory(subdirectory);
+            }
         }
     }
 }
